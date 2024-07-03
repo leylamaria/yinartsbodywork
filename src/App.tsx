86667e1d158b1d83f5e1b5b9145fd64007a3a634
Leyla <157950987+leylamaria@users.noreply.github.com>
2024-07-03 11:29:47 +0200

    Initial commit with vite and tailwind installed

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black">
+      <h1 className="text-3xl font-bold underline text-center py-96 text-[#66E09B]">
+        Stay tuned!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
